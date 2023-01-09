dd0d7b9974f30f42fa668533036f3794b36d128b
Yuan Si <do4suki@gmail.com>
2023-01-10 06:10:13 +0800

    [DNM] oneplus8pro: overlay: Add ParanoidHub
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I0e486e3dab9b1c37d5ac627635fd301fdf4d01d9

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
-    OPlusSystemUIResTarget
+    OPlusSystemUIResTarget \
+    ParanoidHubResTarget
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
